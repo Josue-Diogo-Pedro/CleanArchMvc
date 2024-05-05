d26e4b18a9eeba318a73abc47079918c63eadc35
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-05 14:02:40 -0700

    CleanArchMvc.Domain - finish to implement DomainExceptionValidation

--- src/CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
+++ src/CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -2,5 +2,12 @@
 
 public class DomainExceptionValidation : Exception
 {
+	public DomainExceptionValidation(string error) : base(error)
+	{ }
 
+	public static void When(bool hasError, string error)
+	{
+		if (hasError)
+			throw new DomainExceptionValidation(error);
+	}
 }
