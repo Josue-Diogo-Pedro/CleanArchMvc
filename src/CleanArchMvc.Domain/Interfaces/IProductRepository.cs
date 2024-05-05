59c7dfcb2842bceca7459626ca7d26ba656a2c8d
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-05 16:07:11 -0700

    CleanArchMvc.Domain - finish to implement interface for IProductRepository

---
+++ src/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetByIdAsync(int? id);
+
+    Task<IEnumerable<Product>> GetProductCategoryAsync(int? id);
+
+    Task<Product> CreateAsync(Product product);
+
+    Task<Product> UpdateAsync(Product product);
+    Task<Product> RemoveAsync(Product product);
+
+}
