7a98dc27e7139bd5010c906377e3a3b7087f9a84
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-05 13:53:31 -0700

    CleanArchMvc.Domain - add Category class

---
+++ src/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace CleanArchMvc.Domain.Entities;
+
+public sealed class Category
+{
+    public int CategoryId { get; private set; }
+    public string? Name { get; private set; }
+    public ICollection<Product> Products { get; set; }
+
+    public Category(string name)
+    {
+        Name = name;
+    }
+
+    public Category(int categoryId, string? name)
+    {
+        CategoryId = categoryId;
+        Name = name;
+    }
+
+
+}
