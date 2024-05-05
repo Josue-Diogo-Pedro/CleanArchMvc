1494704235f090766df15610b39c524dfa139af8
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-05 14:07:57 -0700

    CleanArchMvc.Domain - implement private method to ValidateDomain

--- src/CleanArchMvc.Domain/Entities/Category.cs
+++ src/CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,6 @@
-﻿namespace CleanArchMvc.Domain.Entities;
+﻿using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities;
 
 public sealed class Category
 {
@@ -6,10 +8,7 @@ public sealed class Category
     public string? Name { get; private set; }
     public ICollection<Product> Products { get; set; }
 
-    public Category(string name)
-    {
-        Name = name;
-    }
+    public Category(string name) => ValidateDomain(name);
 
     public Category(int categoryId, string? name)
     {
@@ -17,5 +16,13 @@ public sealed class Category
         Name = name;
     }
 
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+
+        DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+        Name = name;
+    }
 
 }
