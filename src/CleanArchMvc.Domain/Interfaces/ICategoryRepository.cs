b94818d57901a7d15210a8d270368f3fa0eab0a0
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-05 15:59:53 -0700

    CleanArchMvc.Domain - finish to create method's assignatures

--- src/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
+++ src/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace CleanArchMvc.Domain.Interfaces;
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces;
 
 public interface ICategoryRepository
 {
+    Task<IEnumerable<Category>> GetCategoriesAsync();
+    Task<Category> GetByIdAsync(int id);
 
+    Task<Category> CreateAsync(Category category);
+    Task<Category> UpdateAsync(Category category);
+    Task<Category> RemoveAsync(Category category);
 }
