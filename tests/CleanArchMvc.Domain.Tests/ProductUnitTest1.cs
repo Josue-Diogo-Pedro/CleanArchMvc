e83ba76daba066ba411a6517669869b3c4ec9771
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-07 14:51:08 -0700

    CleanArchMvc.Domain.Tests - created test CreateProduct_WithValidParameters_ResultObjectValidState

---
+++ tests/CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Validation;
+using FluentAssertions;
+
+namespace CleanArchMvc.Domain.Tests;
+
+public class ProductUnitTest1
+{
+    [Fact(DisplayName = "Create product with valid parameters")]
+    public void CreateProduct_WithValidParameters_ResultObjectValidState()
+    {
+        Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "Product Image");
+        action.Should()
+            .NotThrow<DomainExceptionValidation>();
+    }
+}
