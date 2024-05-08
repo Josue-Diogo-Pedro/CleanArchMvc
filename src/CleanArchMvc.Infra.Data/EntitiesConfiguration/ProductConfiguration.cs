0282527b588dafd20a70ed3c1a01a072e80e9c30
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 11:56:40 -0700

    CleanArchMvc.Infra.Data - finish to implement ProductConfiguration

--- src/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
+++ src/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
-    internal class ProductConfiguration
+    public void Configure(EntityTypeBuilder<Product> builder)
     {
+        builder.HasKey(key => key.Id);
+
+        builder.Property(prop => prop.Name).HasMaxLength(100).IsRequired();
+        builder.Property(prop => prop.Description).HasMaxLength(200).IsRequired();
+        
+        builder.Property(prop => prop.Price).HasPrecision(10,2);
+
+        builder.HasOne(prop => prop.Category)
+            .WithMany(prop => prop.Products).HasForeignKey(fkey => fkey.CategoryId);
+        
     }
 }
