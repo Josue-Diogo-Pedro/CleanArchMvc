923cc4abf94f40b711c083138dbaa961f771191d
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 14:28:44 -0700

    CleanArchMvc.Infra.Data - fix error in migration file when i try to update-database

--- src/CleanArchMvc.Infra.Data/Migrations/20240508211123_AddInitialsProducts.cs
+++ src/CleanArchMvc.Infra.Data/Migrations/20240508211123_AddInitialsProducts.cs
@@ -9,21 +9,21 @@ namespace CleanArchMvc.Infra.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId,CreateDate,ModifiedDate)
                                    VALUES
-                                   ('Caderno Espiral', 'Caderno Espiral 100 folhas', 7.40, 50, 'caderno1.jpg', 1)");
+                                   ('Caderno Espiral', 'Caderno Espiral 100 folhas', 7.40, 50, 'caderno1.jpg', 1, GetDate(),GetDate())");
 
-            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId,CreateDate,ModifiedDate)
                                    VALUES
-                                   ('Estojo Escolar', 'Estojo escolar cinza', 5.65, 70, 'estojo1.jpg', 1)");
+                                   ('Estojo Escolar', 'Estojo escolar cinza', 5.65, 70, 'estojo1.jpg', 1, GetDate(),GetDate())");
 
-            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId,CreateDate,ModifiedDate)
                                    VALUES
-                                   ('Borracha escolar', 'Borracha branca pequena', 3.25, 80, 'borracha1.jpg', 1)");
+                                   ('Borracha escolar', 'Borracha branca pequena', 3.25, 80, 'borracha1.jpg', 1, GetDate(),GetDate())");
 
-            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId,CreateDate,ModifiedDate)
                                    VALUES
-                                   ('Calculadora escolar', 'Calculadora simples', 15.39,20, 'calculadora1.jpg', 2)");
+                                   ('Calculadora escolar', 'Calculadora simples', 15.39,20, 'calculadora1.jpg', 2, GetDate(),GetDate())");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
