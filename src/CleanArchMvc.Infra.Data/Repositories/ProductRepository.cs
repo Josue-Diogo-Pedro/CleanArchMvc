f2756099662efaf381ca3a3c038d34c8b040fb02
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 12:16:44 -0700

    CleanArchMvc.Infra.Data - begin to implement ProductRepository

---
+++ src/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductRepository(ApplicationDbContext context) => _context = context;
+
+    public Task<Product> CreateAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetByIdAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetProductCategoryAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> RemoveAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> UpdateAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+}
