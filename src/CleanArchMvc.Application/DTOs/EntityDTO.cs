6df46087094d8fe25e2f46f8f3eb44aa5bcd9534
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 15:01:48 -0700

    CleanArchMvc.Application - create EntityDTO

---
+++ src/CleanArchMvc.Application/DTOs/EntityDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchMvc.Application.DTOs;
+
+public class EntityDTO
+{
+    public int Id { get; protected set; }
+
+    public DateTime CreateDate { get; set; } = DateTime.Now;
+    public DateTime ModifiedDate { get; set; } = DateTime.Now;
+    public string? CreateBy { get; set; }
+    public string? ModifiedBy { get; set; }
+}
