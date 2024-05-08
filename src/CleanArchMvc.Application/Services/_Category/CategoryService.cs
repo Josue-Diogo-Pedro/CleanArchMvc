dba5aec51f9418538245876abeaab2f434808bf6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 15:24:35 -0700

    CleanArchMvc.Application - part of implementation of CategoryService

--- src/CleanArchMvc.Application/Services/_Category/CategoryService.cs
+++ src/CleanArchMvc.Application/Services/_Category/CategoryService.cs
@@ -1,11 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Interfaces;
 
 namespace CleanArchMvc.Application.Services._Category;
 
-internal class CategoryService
+public class CategoryService : ICategoryService
 {
+	private readonly ICategoryRepository _categoryRepository;
+	private readonly IMapper _mapper;
+
+	public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+	{
+        _categoryRepository = categoryRepository;
+		_mapper = mapper;
+	}
+
+	public async Task<IEnumerable<CategoryDTO>> GetCategoriesAsync()
+	{
+		var categories = await _categoryRepository.GetCategoriesAsync();
+		return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
+	}
+
+	public async Task<CategoryDTO> GetByIdAsync(int? id)
+	{
+		var category = await _categoryRepository.GetByIdAsync(id);
+		return _mapper.Map<CategoryDTO>(category);
+	}
+
+	public async Task<CategoryDTO> CreateAsync(CategoryDTO category)
+	{
+		
+	}
+
+	public Task<CategoryDTO> UpdateAsync(CategoryDTO category)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<CategoryDTO> RemoveAsync(CategoryDTO category)
+	{
+		throw new NotImplementedException();
+	}
+
 }
