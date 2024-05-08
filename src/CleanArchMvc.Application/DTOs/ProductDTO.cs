6bf31e13627072a235fd811bf9b3fe89afb50494
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 15:09:11 -0700

    CleanArchMvc.Application - finish to create ProductDTO

---
+++ src/CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Application.DTOs;
+
+public class ProductDTO : EntityDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Image { get; set; }
+    public Category Category { get; set; }
+    public int CategoryId { get; set; }
+}
