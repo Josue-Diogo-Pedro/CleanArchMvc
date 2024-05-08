51ce83fbd9c6bb290a075e263b9857900ce65423
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 11:11:31 -0700

    CleanArchMvc.Infra.Data . begin to implement ProductConfiguration

---
+++ src/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    internal class ProductConfiguration
+    {
+    }
+}
