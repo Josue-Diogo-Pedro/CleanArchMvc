3a8a03c97dbbf18e78a6218c5870884a26011691
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 14:21:48 -0700

    CleanArchMvc.Infra.Data - add new migration to populate some Products

---
+++ src/CleanArchMvc.Infra.Data/Migrations/20240508211123_AddInitialsProducts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class AddInitialsProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+                                   VALUES
+                                   ('Caderno Espiral', 'Caderno Espiral 100 folhas', 7.40, 50, 'caderno1.jpg', 1)");
+
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+                                   VALUES
+                                   ('Estojo Escolar', 'Estojo escolar cinza', 5.65, 70, 'estojo1.jpg', 1)");
+
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+                                   VALUES
+                                   ('Borracha escolar', 'Borracha branca pequena', 3.25, 80, 'borracha1.jpg', 1)");
+
+            migrationBuilder.Sql(@"INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)
+                                   VALUES
+                                   ('Calculadora escolar', 'Calculadora simples', 15.39,20, 'calculadora1.jpg', 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FORM Products");
+        }
+    }
+}
