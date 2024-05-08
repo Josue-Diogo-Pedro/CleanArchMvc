868fd4cdb4a6b1dc7560a2a098e8c3e085afbb46
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 11:10:18 -0700

    CleanArchMvc.Infra.Data - finish to implement CategoryConfiguration

--- src/CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
+++ src/CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -1,5 +1,21 @@
-﻿namespace CleanArchMvc.Infra.Data.EntitiesConfiguration;
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class CategoryConfiguration
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(key => key.Id);
+        builder.Property(prop => prop.Name).HasMaxLength(100).IsRequired();
+
+        builder.HasData(
+            new Category(1, "Material Escolar"),
+            new Category(2, "Eletrônicos"),
+            new Category(3, "Acessórios")
+            );
+
+    }
 }
