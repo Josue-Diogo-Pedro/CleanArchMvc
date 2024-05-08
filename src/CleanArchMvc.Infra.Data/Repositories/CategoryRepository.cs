2808bc66dfa44dc29d203da8f7589e52a8409469
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 12:09:22 -0700

    CleanArchMvc.Infra.Data - part of implementation of CategoryRepository

--- src/CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
+++ src/CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -1,5 +1,42 @@
-﻿namespace CleanArchMvc.Infra.Data.Repositories;
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
-internal class CategoryRepository
+namespace CleanArchMvc.Infra.Data.Repositories;
+
+public class CategoryRepository : ICategoryRepository
 {
+    private readonly ApplicationDbContext _context;
+
+    public CategoryRepository(ApplicationDbContext context) => _context = context;
+
+    public async Task<IEnumerable<Category>> GetCategoriesAsync() => await _context.Categories?
+                                                                            .AsNoTracking()?
+                                                                            .DefaultIfEmpty()?
+                                                                            .ToListAsync();
+
+    public async Task<Category> GetByIdAsync(int? id) => await _context.Categories?
+                                                                .AsNoTracking()?
+                                                                .DefaultIfEmpty()?
+                                                                .SingleOrDefaultAsync(category => category.Id == id);
+    
+    public Task<Category> CreateAsync(Category category)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<Category> UpdateAsync(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+    public Task<Category> RemoveAsync(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
 }
