48c4f7f93936720a06947c4f3a53f80c39d7e1b0
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 12:48:25 -0700

    CleanArchMvc.Infra.IoC - finish to implement DependencyInjection

--- src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,8 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
 
 namespace CleanArchMvc.Infra.IoC;
 
 public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)
+                                );
+        });
+
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        return services;
+    }
 }
