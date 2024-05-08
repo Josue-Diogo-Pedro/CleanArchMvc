58bb6d28eac31a39e4b7e836fa29ac9798c27950
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-08 10:51:51 -0700

    CleanArchMvc.Infra.Data - partial finish on ApplicationDbContext

--- src/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ src/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace CleanArchMvc.Infra.Data.Context
+namespace CleanArchMvc.Infra.Data.Context;
+
+public class ApplicationDbContext : DbContext
 {
-    internal class ApplicationDbContext
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
