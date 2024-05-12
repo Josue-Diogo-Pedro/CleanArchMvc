bfa97da127248673477cbd971e82c221fdd4c4d0
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-12 15:19:25 -0700

    CleanArchMvc.Application - part of implementation of ProductCreateCommnadHandler

--- src/CleanArchMvc.Application/Products/Handlers/ProductCreateCommnadHandler.cs
+++ src/CleanArchMvc.Application/Products/Handlers/ProductCreateCommnadHandler.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
 
 namespace CleanArchMvc.Application.Products.Handlers;
 
-internal class ProductCreateCommnadHandler
+public class ProductCreateCommnadHandler : IRequestHandler<ProductCreateCommnad, Product>
 {
+    private readonly IProductRepository _productRepository;
+
+    public ProductCreateCommnadHandler(IProductRepository productRepository) => _productRepository = productRepository;
+
+    public Task<Product> Handle(ProductCreateCommnad request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
