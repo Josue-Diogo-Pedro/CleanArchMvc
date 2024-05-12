ea4d70f96b3abb5b529c5648cb5303d989fe6c3c
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-12 15:24:04 -0700

    CleanArchMvc.Application - finish to create ProductCreateCommnadHandler class

--- src/CleanArchMvc.Application/Products/Handlers/ProductCreateCommnadHandler.cs
+++ src/CleanArchMvc.Application/Products/Handlers/ProductCreateCommnadHandler.cs
@@ -11,8 +11,16 @@ public class ProductCreateCommnadHandler : IRequestHandler<ProductCreateCommnad,
 
     public ProductCreateCommnadHandler(IProductRepository productRepository) => _productRepository = productRepository;
 
-    public Task<Product> Handle(ProductCreateCommnad request, CancellationToken cancellationToken)
+    public async Task<Product> Handle(ProductCreateCommnad request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var product = new Product(request.Name, request.Description, request.Price.Value, request.Stock, request.Image);
+
+        if (product is null)
+            throw new ApplicationException("Error when try to create entity");
+        else
+        {
+            product.CategoryId = request.CategoryId;
+            return await _productRepository.CreateAsync(product);
+        }
     }
 }
