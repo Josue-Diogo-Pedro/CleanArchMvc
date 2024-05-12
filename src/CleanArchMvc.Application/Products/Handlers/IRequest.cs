9a56d6943527b4667f150916a361387d0f985ca4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-12 15:35:04 -0700

    CleanArchMvc.Application - part of implementation of ProductRemoveCommandHandler class

---
+++ src/CleanArchMvc.Application/Products/Handlers/IRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanArchMvc.Application.Products.Handlers
+{
+    internal interface IRequest<T1, T2>
+    {
+    }
+}
--- src/CleanArchMvc.Application/Products/Handlers/ProductRemoveCommnadHandler.cs
+++ src/CleanArchMvc.Application/Products/Handlers/ProductRemoveCommnadHandler.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
 
-namespace CleanArchMvc.Application.Products.Handlers
+namespace CleanArchMvc.Application.Products.Handlers;
+
+public class ProductRemoveCommnadHandler : IRequestHandler<ProductRemoveCommand, Product>
 {
-    internal class ProductRemoveCommnadHandler
+    private readonly IProductRepository _productRepository;
+
+    public ProductRemoveCommnadHandler(IProductRepository productRepository) => _productRepository = productRepository;
+
+    public Task<Product> Handle(ProductRemoveCommand request, CancellationToken cancellationToken)
     {
+        throw new NotImplementedException();
     }
 }
