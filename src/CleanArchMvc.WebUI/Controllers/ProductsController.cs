0f9a10dc9676074ff0a21267b7277a0dc0f4c99f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-12 03:10:41 -0700

    CleanArchMvc.WebUI - add action Index in ProductsController

--- src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CleanArchMvc.Application.Services._Product;
+using Microsoft.AspNetCore.Mvc;
 
-namespace CleanArchMvc.WebUI.Controllers
+namespace CleanArchMvc.WebUI.Controllers;
+
+public class ProductsController : Controller
 {
-    public class ProductsController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService) => _productService = productService;
+
+    [HttpGet]
+    public async Task<ActionResult> Index() => View(await _productService.GetProductsAsync());
 }
