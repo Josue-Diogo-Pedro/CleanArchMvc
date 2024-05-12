5ec59467a3cd250fc740fc5fcf82866073fd4807
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-12 02:43:56 -0700

    CleanArchMvc.Infra.IoC - finish to register services

--- src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ src/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using CleanArchMvc.Domain.Interfaces;
+﻿using AutoMapper;
+using CleanArchMvc.Application.AutoMapper;
+using CleanArchMvc.Application.Services._Category;
+using CleanArchMvc.Application.Services._Product;
+using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +16,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
 
+        //Connection String and Context register
         services.AddDbContext<ApplicationDbContext>(options =>
         {
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
@@ -19,8 +24,22 @@ public static class DependencyInjection
                                 );
         });
 
+        //Automapper services register
+        var mappingConfig = new MapperConfiguration(options =>
+        {
+            options.AddProfile(new MappingProfile());
+        });
+
+        IMapper mapper = mappingConfig.CreateMapper();
+        services.AddSingleton(mapper);
+
+        //Repositories services register
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
+
+        //Services "services" register (funny yes?! rsr)
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IProductService, ProductService>();
 
         return services;
     }
