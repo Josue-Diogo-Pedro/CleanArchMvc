7f251dba81c7104cebea6c5d84c962cf25157a10
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-12 15:02:39 -0700

    CleanArchMvc.Application - create ProcuctRemoveCommand class

---
+++ src/CleanArchMvc.Application/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchMvc.Application.Products.Commands;
+
+public class ProductRemoveCommand : ProductCommand
+{
+    public int Id { get; set; }
+
+	public ProductRemoveCommand(int id) => Id = id;
+}
