7e0cb726b6ac0a80094beb110c7279da5a3ecf84
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-13 13:36:17 -0700

    CleanArchMvc.Application - finish to implement CategoryRemoveCommandHandler class

--- src/CleanArchMvc.Application/Categories/Handlers/CategoryRemoveCommandHandler.cs
+++ src/CleanArchMvc.Application/Categories/Handlers/CategoryRemoveCommandHandler.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Application.Categories.Commnads;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
 
-namespace CleanArchMvc.Application.Categories.Handlers
+namespace CleanArchMvc.Application.Categories.Handlers;
+
+public class CategoryRemoveCommandHandler : IRequestHandler<CategoryUpdateCommand, Category>
 {
-    internal class CategoryRemoveCommandHandler
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryRemoveCommandHandler(ICategoryRepository categoryRepository) => _categoryRepository = categoryRepository;
+
+    public async Task<Category> Handle(CategoryUpdateCommand request, CancellationToken cancellationToken)
     {
+        var category = await _categoryRepository.GetByIdAsync(request.Id);
+
+        if (category is null)
+            throw new ApplicationException("Error, cold not be found");
+        else
+            return await _categoryRepository.RemoveAsync(category);
     }
 }
