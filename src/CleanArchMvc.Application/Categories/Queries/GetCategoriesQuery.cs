ad1d2aea81b15dad745162f7384db15423777c13
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-13 13:04:30 -0700

    CleanArchMvc.Application - create GetCategoriesQuery class

---
+++ src/CleanArchMvc.Application/Categories/Queries/GetCategoriesQuery.cs
@@ -0,0 +1,8 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Categories.Queries;
+
+public class GetCategoriesQuery : IRequest<IEnumerable<Category>>
+{
+}
