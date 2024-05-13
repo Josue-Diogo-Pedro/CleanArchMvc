92e2668183def6ecf7fd2e076215f374972599f1
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-13 12:29:50 -0700

    CleanArchMvc.WebUI - add buttons to view products and categories

--- src/CleanArchMvc.WebUI/Program.cs
+++ src/CleanArchMvc.WebUI/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Products}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
