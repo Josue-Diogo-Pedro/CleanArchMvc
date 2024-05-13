94a99e8259b9768c805d24e4b1a339cfc5355986
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-13 13:07:08 -0700

    CleanArchMvc.Application - begin to implement GetCategoryByIdHandler class

---
+++ src/CleanArchMvc.Application/Categories/Handlers/GetCategoryByIdHandler.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Application.Categories.Queries;
+using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Categories.Handlers;
+
+public class GetCategoryByIdHandler : IRequestHandler<GetCategoryByIdQuery, Category>
+{
+    
+    public Task<Category> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
