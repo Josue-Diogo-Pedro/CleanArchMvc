bd913292ab1b6ceb6faee670b879c8562fc44729
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-13 13:12:58 -0700

    CleanArchMvc.Application - finish to implement GetCategoriesHandler class

--- src/CleanArchMvc.Application/Categories/Handlers/GetCategoriesHandler.cs
+++ src/CleanArchMvc.Application/Categories/Handlers/GetCategoriesHandler.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Application.Categories.Queries;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
 
-namespace CleanArchMvc.Application.Categories.Handlers
+namespace CleanArchMvc.Application.Categories.Handlers;
+
+public class GetCategoriesHandler : IRequestHandler<GetCategoriesQuery, IEnumerable<Category>>
 {
-    internal class GetCategoriesHandler
-    {
-    }
+    private readonly ICategoryRepository _categoryRepository;
+
+    public GetCategoriesHandler(ICategoryRepository categoryRepository) => _categoryRepository = categoryRepository;
+
+    public async Task<IEnumerable<Category>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken) =>
+        await _categoryRepository.GetCategoriesAsync();
 }
