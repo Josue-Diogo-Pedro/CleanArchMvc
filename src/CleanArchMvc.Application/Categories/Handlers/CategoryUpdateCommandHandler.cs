c23f00f33f16703236a4d2b4b1a130615b7a307f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-13 13:31:28 -0700

    CleanArchMvc.Application - finish to implement CategoryUpdateCommandHandler class

--- src/CleanArchMvc.Application/Categories/Handlers/CategoryUpdateCommandHandler.cs
+++ src/CleanArchMvc.Application/Categories/Handlers/CategoryUpdateCommandHandler.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Application.Categories.Commnads;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
 
-namespace CleanArchMvc.Application.Categories.Handlers
+namespace CleanArchMvc.Application.Categories.Handlers;
+
+public class CategoryUpdateCommandHandler : IRequestHandler<CategoryUpdateCommand, Category>
 {
-    internal class CategoryUpdateCommandHandler
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryUpdateCommandHandler(ICategoryRepository categoryRepository) => _categoryRepository = categoryRepository;
+
+    public async Task<Category> Handle(CategoryUpdateCommand request, CancellationToken cancellationToken)
     {
+        Category category = await _categoryRepository.GetByIdAsync(request.Id);
+
+        if (category is null)
+            throw new ApplicationException("Error, cold not be found");
+        else
+        {
+            category.Update(request.Name);
+            return await _categoryRepository.UpdateAsync(category);
+        }
     }
 }
