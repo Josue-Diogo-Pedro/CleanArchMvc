b531c224f43eb10e9af13ef592bcccf34f316703
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-14 13:06:36 -0700

    CleanArchMvc.WebUI - in ProductsController, action to show view create "created(rsr)"

--- src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ src/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,17 +1,29 @@
-﻿using CleanArchMvc.Application.Services._Product;
+﻿using CleanArchMvc.Application.Services._Category;
+using CleanArchMvc.Application.Services._Product;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMvc.WebUI.Controllers;
 
 public class ProductsController : Controller
 {
     private readonly IProductService _productService;
+    private readonly ICategoryService _categoryService;
 
-    public ProductsController(IProductService productService) => _productService = productService;
+    public ProductsController(IProductService productService, ICategoryService categoryService)
+    {
+        _productService = productService;
+        _categoryService = categoryService;
+    }
 
     [HttpGet]
     public async Task<ActionResult> Index() => View(await _productService.GetProductsAsync());
 
     [HttpGet]
-    public IActionResult Create() => View();
+    public async Task<ActionResult> Create()
+    {
+        ViewBag.CategoryId = new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name");
+        return View();
+    }
+
 }
