8597068ea98e8826e5993094e59ff23a09e6c01f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-14 22:46:45 -0700

    CleanArchMvc.WebUI - add Identity class in context

--- src/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ src/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.Data.Context;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public DbSet<Category>? Categories { get; set; }
     public DbSet<Product>? Products { get; set; }
---
+++ src/CleanArchMvc.Infra.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchMvc.Infra.Data.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+}
