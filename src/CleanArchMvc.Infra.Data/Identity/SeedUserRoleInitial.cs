92d92e9a4891cc2c9e339b2c72636d1c54ff8819
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-15 14:28:31 -0700

    CleanArchMvc.Infra.Data - create class SeedUserRoldeInitial and implement constructor and base property

---
+++ src/CleanArchMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchMvc.Infra.Data.Identity;
+
+public class SeedUserRoleInitial
+{
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public SeedUserRoleInitial(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+}
