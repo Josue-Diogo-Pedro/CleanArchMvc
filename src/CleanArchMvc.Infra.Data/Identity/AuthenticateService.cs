ac07cb672516c26d1b688c3f4c1c49bc0d3476e9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-15 14:05:45 -0700

    CleanArchMvc.Infra.Data - add AuthenticateService class and implement methods for IAuthenticate

---
+++ src/CleanArchMvc.Infra.Data/Identity/AuthenticateService.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Domain.Account;
+
+namespace CleanArchMvc.Infra.Data.Identity;
+
+public class AuthenticateService : IAuthenticate
+{
+    public Task<bool> Authenticate(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Logout()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RegisterUser(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
