b2a2ac49f1ff57643ee786e08a4515d40f95bbc8
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-16 13:47:08 -0700

    CleanArchMvc.WebUI - begin to create AccountController

---
+++ src/CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly IAuthenticate _authenticate;
+
+    public AccountController(IAuthenticate authenticate) => _authenticate = authenticate;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
