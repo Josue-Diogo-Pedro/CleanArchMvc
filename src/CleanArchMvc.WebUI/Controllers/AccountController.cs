845388e2229ad86d8a77ae060020ac6239fa1872
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-16 13:59:51 -0700

    CleanArchMvc.WebUI - action to register user added

--- src/CleanArchMvc.WebUI/Controllers/AccountController.cs
+++ src/CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -32,5 +32,17 @@ public class AccountController : Controller
     [HttpGet]
     public IActionResult Register() => View();
 
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel registerViewModel)
+    {
+        var result = await _authenticate.RegisterUser(registerViewModel.Email??string.Empty, registerViewModel.Password??string.Empty);
 
+        if (result) 
+            return Redirect("/");
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Invalid register attempt(password must be strong)");
+            return View(registerViewModel);
+        }
+    }
 }
