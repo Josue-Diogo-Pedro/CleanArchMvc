63527fcc63155ddb108420adce0c5b9a81f255ce
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-16 13:38:45 -0700

    CleanArchMvc.WebUI - ViewModel for Register created

---
+++ src/CleanArchMvc.WebUI/Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebUI.Models;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "The password is required")]
+    [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string? ConfirmPassword { get; set; }
+}
